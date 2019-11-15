d2e538d0f1acc9d0ebf62b93b2fa08a964804832
Janus Bo Andersen <janus@janusboandersen.dk>
2019-11-16 00:39:38 +0800

    add folder for test cases and U1, U2

---
+++ test-cases/u2-tc-1.sh
@@ -0,0 +1,51 @@
+echo "Running testcase: U1.TC-1"
+echo "Verifikation af at Django på Docker fungerer på lokalmaskine"
+echo "Linux og Mac"
+
+URL=http://localhost
+URL_WIN=http://192.168.99.100
+PORT=8000
+
+# Prepare
+if [[ `uname` =~ "MING"? ]];then
+  USER_OS="Windows"
+elif [[ `uname` =~ "Darwin"? ]];then
+  USER_OS="macOS"
+elif [[ `uname` =~ "Linux"? ]];then
+  USER_OS="Linux"
+else
+  USER_OS="unknown"
+fi
+
+# Set up to ensure fresh start
+docker-compose down
+docker volume rm data-volume
+docker volume rm mqtt-volume
+docker volume create data-volume
+docker volume create mqtt-volume
+
+# Get updated code from relevant branch
+# git pull
+# git checkout master
+
+# Perform test steps
+echo "*** TEST STEPS ***"
+echo "Running containers in interactive mode with logging"
+echo "Please quit with CTRL-C when done viewing output"
+echo "*** VERIFY NO ERRORS ***"
+sleep 5.0
+docker-compose up --build
+docker-compose down
+
+echo "*** RE-STARTING SERVICES AND BROWSER ***"
+docker-compose up -d
+sleep 2.0
+if [ "${USER_OS}" == "Windows" ];then
+  start chrome $URL_WIN:$PORT
+elif [ "${USER_OS}" == "macOS" ];then
+  open "${URL}:${PORT}"
+elif [ "${USER_OS}" == "Linux" ];then
+  xdg-open "${URL}:${PORT}"
+else
+  echo "${USER_OS} OS, Please manually open browser on: ${URL}:${PORT}"
+fi
