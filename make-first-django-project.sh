7420e61970e17e03f1f1e9cf445a1ee685806951
Janus Bo Andersen <janus@janusboandersen.dk>
2019-11-15 13:19:34 +0800

    Update startup script

--- make-first-django-project.sh
+++ make-first-django-project.sh
@@ -3,14 +3,14 @@
 # Make a docker volume for the database
 docker volume create data-volume
 
-# Build and start the containers
-docker-compose up --build
+# Build the containers
+docker-compose build
 
-# Create the project
-docker-compose exec webinterface django-admin.py startproject webinterface .
+# Create the Django project files
+docker-compose run webinterface sh -c "django-admin.py startproject webinterface ."
 
-# Migrate the database for the new app
+# Migrate the database for the new project
 docker-compose exec webinterface python manage.py migrate --noinput
 
 # Create a superuser for the admin role
-docker-compose exec webinterface python manage.py createsuperuser --username admin --email test@test.dk
+docker-compose exec webinterface python manage.py createsuperuser --username ditnavn --email dit@navn.dk
