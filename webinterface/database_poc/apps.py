8f593cce3029e85767a461aea48f793266fb2ebe
Blunsdon <blunsdon@hotmail.dk>
2020-02-19 17:53:42 +0100

    Only messagehandler is needed

--- webinterface/database_poc/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-# App definition
-class DatabasePocConfig(AppConfig):
-    name = 'database_poc'
--- webinterface/demo_module/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DemoModuleConfig(AppConfig):
-    name = 'demo_module'
