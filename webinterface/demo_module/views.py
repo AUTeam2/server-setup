0b6a46f1f44c31925b9198083575fc4381e93f39
Janus Bo Andersen <janus@janusboandersen.dk>
2020-02-24 00:53:45 +0800

    Update UI for tb5 with ListViews for Result and Status

--- webinterface/demo_module/views.py
+++ webinterface/demo_module/views.py
@@ -5,10 +5,14 @@ URL paths that lead here are in demo_module/urls.py
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView
+from django.template import loader
+
 from demo_module.messagehandler.client import MqttClient
 from demo_module.messagehandler import protocol
-from django.template import loader
 from .forms import TestForm
+from .models import Result, Status
+
 import json
 
 # Show landing page for the demo module
@@ -105,47 +109,58 @@ def transmit_mqtt(form_obj):
     return rc
 
 
-def send_mqtt(request):
+class ResultListView(ListView):
+    model = Result
+    queryset = Result.objects.all().order_by('-job_received_time')
 
-    # Create a message to send
-    topic = "demo_module/inbound"
 
-    # Payload
-    m = protocol.Message()
-    m.new()
-    m.sentBy = "Team 2"
-    m.msgType = "data"
-    m.statusCode = "200"
-    m.dataObj = {
-        'x': [1,2,3,4,5,6,7,8,9,10],
-        'y': [1,4,9,16,25,36,49,64,81,100]
-    }
+class StatusListView(ListView):
+    model = Status
+    queryset = Status.objects.all()
 
-    # Get ready to send
-    m.pack()
-    send_me = protocol.ProtocolSchema.write_jsonstr(m.payload)
 
-    # The donothing function
-    def donothing(client, userdata, message):
-        pass
+# def send_mqtt(request):
+#
+#     # Create a message to send
+#     topic = "demo_module/inbound"
+#
+#     # Payload
+#     m = protocol.Message()
+#     m.new()
+#     m.sentBy = "Team 2"
+#     m.msgType = "data"
+#     m.statusCode = "200"
+#     m.dataObj = {
+#         'x': [1,2,3,4,5,6,7,8,9,10],
+#         'y': [1,4,9,16,25,36,49,64,81,100]
+#     }
+#
+#     # Get ready to send
+#     m.pack()
+#     send_me = protocol.ProtocolSchema.write_jsonstr(m.payload)
+#
+#     # The donothing function
+#     def donothing(client, userdata, message):
+#         pass
+#
+#     # Create client
+#     publisher = MqttClient("MessageSender", donothing)
+#
+#     # Send and disconnect
+#     rc = publisher.publish(topic, send_me)
+#     publisher.disconnect()
+#
+#     # Render a response
+#     template = loader.get_template('demo_module/message_sent.html')
+#
+#     if rc:
+#         outcome = "Success. Message was sent."
+#     else:
+#         outcome = "Failure. Message was not sent."
+#
+#     context = {'outcome': outcome,
+#                'topic': topic,
+#                'msg': send_me}
+#
+#     return HttpResponse(template.render(context, request))
 
-    # Create client
-    publisher = MqttClient("MessageSender", donothing)
-
-    # Send and disconnect
-    rc = publisher.publish(topic, send_me)
-    publisher.disconnect()
-
-    # Render a response
-    template = loader.get_template('demo_module/message_sent.html')
-
-    if rc:
-        outcome = "Success. Message was sent."
-    else:
-        outcome = "Failure. Message was not sent."
-
-    context = {'outcome': outcome,
-               'topic': topic,
-               'msg': send_me}
-
-    return HttpResponse(template.render(context, request))
