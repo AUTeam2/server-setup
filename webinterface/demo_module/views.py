6bdae209e5ab42312b8d79c7756f6050f9795edb
Gubimand <malthe@bivs.dk>
2020-02-24 15:55:42 +0100

    Added lines 8-9 in views.py

--- webinterface/demo_module/views.py
+++ webinterface/demo_module/views.py
@@ -5,44 +5,53 @@ from demo_module.messagehandler import protocol
 from django.template import loader
 
 
+# remember to implement database import in views, when database gets implemented
+# from . models import database_tests, datatypes, datapoints
+
+
 # Create your views here.
 
-#main demo_module page
+# main demo_module page
 def index(request):
-    return render(request,'/demo_module/templates/index.html')
+    return render(request, '/demo_module/templates/index.html')
 
-#start/create test page
+
+# start/create test page
 def start_test(request):
     return render(request, '/demo_module/templates/start_test.html')
 
-#show running test page
+
+# show running test page
 def running_test(request):
     return render(request, '/demo_module/templates/running_test.html')
 
-#show saved test page
+
+# show saved test page
 def saved_data(request):
-    #import all test, when database is implementet uncomment these
-    #all_test = Date_list.objects.all()
-    #context = {"all_test" : all_test}
-    #return render(request, '/demo_module/templates/saved_data.html',context)
+    # import all test, when database is implementet uncomment these
+    # all_test = Date_list.objects.all()
+    # context = {"all_test" : all_test}
+    # return render(request, '/demo_module/templates/saved_data.html',context)
     return render(request, '/demo_module/templates/saved_data.html')
 
-#Show specific data page, specific datapoints from a test from saved_data
-def show_data(request,data_id):
-    #import specific test data, when database is implementet uncomment these
-    #datatype_list = date_list.objects.get(pk= 'primary_key')
-    #datapoints = datapoints.objects.filter(collection=datatype_list)
-    #context = {"datapoints" : datapoints}
-    #return render(request, '/demo_module/templates/show_data.html',context)
-    return render(request, '/demo_module/templates/show_data.html',)
 
-#busy page, test already running
+# Show specific data page, specific datapoints from a test from saved_data
+def show_data(request, data_id):
+    # import specific test data, when database is implementet uncomment these
+    # datatype_list = date_list.objects.get(pk= 'primary_key')
+    # datapoints = datapoints.objects.filter(collection=datatype_list)
+    # context = {"datapoints" : datapoints}
+    # return render(request, '/demo_module/templates/show_data.html',context)
+    return render(request, '/demo_module/templates/show_data.html', )
+
+
+# busy page, test already running
 def busy(request):
     return render(request, '/demo_module/templates/busy.html')
 
-#mqtt handler
-def send_mqtt(request):
 
+# mqtt handler
+def send_mqtt(request):
     # Create a message to send
     topic = "demo_module/inbound"
 
@@ -53,8 +62,8 @@ def send_mqtt(request):
     m.msgType = "data"
     m.statusCode = "200"
     m.dataObj = {
-        'x': [1,2,3,4,5,6,7,8,9,10],
-        'y': [1,4,9,16,25,36,49,64,81,100]
+        'x': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        'y': [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
     }
 
     # Get ready to send
